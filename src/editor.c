4833b6976d938f7884d292c5472962776a31dfca
lambda-src <dubstepaztec35@gmail.com>
2026-01-08 12:28:23 -0800

    made a simple dynamic text buffer :3

--- src/editor.c
+++ src/editor.c
@@ -1,4 +1,12 @@
 #include "editor.h"
+#include <stdlib.h>
+#include <string.h>
+
+// This will prob need to be reworked since ncurses fucks the terminal 
+void panic(const uint8_t *err) {
+    fprintf(stderr, "%s\n", err);
+    exit(EXIT_FAILURE);
+}
 
 void editor_start() {
 	raw();
@@ -32,6 +40,7 @@ void editor_loop(Editor *editor) {
 
 			case KEY_BACKSPACE: 
 				if(move(editor->cur.y, editor->cur.x-1) == OK)
+                    // TODO characters need to be deleted based upon cursor position
 					delch();
 				break;
 			//Attribs
@@ -46,8 +55,64 @@ void editor_loop(Editor *editor) {
 				break;
 			default: 
 				printw("%c", editor->kb_buffer);
+                // TODO charcters need to be inserted into the ed_buffer based upon cursor pos
 				break;
         }
         refresh();
     }
+}
+
+EditorBuffer *init_editor_buffer() {
+    EditorBuffer *buff = malloc(sizeof(EditorBuffer));
+    if (buff == NULL)
+        panic("Allocation error");
+    buff->ptr = malloc(INITIAL_CAP);
+    if (buff->ptr == NULL)
+        panic("Allocation error");
+    buff->cap = INITIAL_CAP;
+    buff->len = 0;
+    return buff;
+}
+
+void editor_buffer_grow(EditorBuffer *buff) {
+    // Blazingly fast way to multiply by 2
+    buff->cap <<= 1; 
+    uint8_t *new_buff = realloc(buff->ptr, buff->cap);
+    if (new_buff == NULL) 
+        // If the allocation fails cause the buffer got too big then ideally we would 
+        // save the contents to a file or something or rework the editor buffer to not
+        // suck as much :c 
+        panic("Allocation error");
+    buff->ptr = new_buff;
+}
+
+void editor_buffer_push(EditorBuffer *buff, uint8_t byte) {
+    if (buff->len == buff->cap)
+        editor_buffer_grow(buff);
+    buff->ptr[buff->len++] = byte;
+}
+
+void editor_buffer_insert(EditorBuffer *buff, size_t index, uint8_t byte) {
+    if (buff->len == buff->cap)
+        editor_buffer_grow(buff);
+    // Shift the elements to make room for the new element 
+    memmove(
+        &buff->ptr[index], 
+        &buff->ptr[index+1], 
+        &buff->ptr[buff->len] - &buff->ptr[index]
+    );
+    buff->ptr[index] = byte;
+    buff->len++;
+}
+
+void editor_buffer_remove(EditorBuffer *buff, size_t index) {
+    // If any of these early return
+    if (buff->len == 0 || index < 0 || index >= buff->len)
+        return;
+    memmove(
+        &buff->ptr + index, 
+        &buff->ptr + index + 1, 
+        (buff->len - index - 1)
+    );
+    buff->len--;
 }
--- src/editor.h
+++ src/editor.h
@@ -7,7 +7,23 @@
     .kb_buffer = EOF, \ 
     .cur = (Cursor){.x = 0, .y = 0}, \
     .win = initscr() \
+    .ed_buffer = init_editor_buffer() \
 })
+
+// The intial capacity for the editing buffer
+#define INITIAL_CAP 1024
+
+// This will most def need to be re worked at some point but for now its gonna be a simple 
+// dynamic array implementation
+typedef struct {
+    size_t cap; 
+    size_t len;
+    // Even though this is an array of bytes there's no need for a \0 at the end because we already know the length
+    // Also this mayyyy need to be changed for the future if you had unicode characters that were more then 1 byte
+    // since if you had this string: "hi 🐼!" the actual byte length is 9 so deleting the 3th byte wont delete the 
+    // emoji and will instead delete the first byte of the emoji sequence
+    uint8_t *ptr;
+} EditorBuffer;
 
 typedef struct {
     int8_t x;
@@ -18,6 +34,7 @@ typedef struct {
     Cursor cur;
     int16_t kb_buffer; 
     WINDOW *win;
+    EditorBuffer *ed_buffer;
 } Editor;
 
 // During this all of the setup stuff should happen
@@ -27,4 +44,22 @@ void editor_start();
 // This is the infinite loop of waiting for user input and doing stuff with it 
 void editor_loop(Editor *editor);
 
+// Allocate a editor buffer
+// This probably doesn't need to be freed since when the program exits the editor will be freed
+EditorBuffer *init_editor_buffer();
 
+// Push a byte into the buffer and grow if need be 
+void editor_buffer_push(EditorBuffer *buff, uint8_t byte);
+
+// Double the size of the buffers capacity and realloc the current buffer over
+void editor_buffer_grow(EditorBuffer *buff);
+
+// Insert a character at a specific index and shift stuff accordingly
+void editor_buffer_insert(EditorBuffer *buff, size_t index, uint8_t byte);
+
+// Deletes a character then shifts everything accordingly
+void editor_buffer_remove(EditorBuffer *buff, size_t index);
+
+// Print a error message to stderr then exit the program
+// This is only for unrecoverable errors 
+void panic(const uint8_t *err);
